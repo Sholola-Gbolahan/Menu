05e8dde586961f85a40f0a40c9c3d8c783cd9dc3
Gbolahan <shololagbolahan@gmail.com>
2024-06-06 13:43:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Title from "./Title";
+
+const App = () => {
+  return (
+    <main>
+      <section className="menu">
+        <Title text="Our Menu" />
+      </section>
+    </main>
+  );
+};
+export default App;
