5e56f775ae6feab1823f824d5b0a4305236f6da1
Gbolahan <shololagbolahan@gmail.com>
2024-06-10 12:39:41 +0100

    Menu Tabs display setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
 const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
@@ -9,12 +10,11 @@ const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
 
-  console.log(categories);
-
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
